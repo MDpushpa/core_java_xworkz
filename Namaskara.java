fafb8e8ccd0ac64c411ab189aa16a35feac43217
mdpushpa <pushpamd575@gmail.com>
2023-07-13 14:31:14 +0530

    adding Namaakara

---
+++ Namaskara.java
@@ -0,0 +1,17 @@
+class  Namaskara {
+     public static void main(String arg[]){
+	      System.out.println("hi i am good");
+          System.out.println("Namaskara is ending");{
+			  
+		  }
+    }
+}
+//
+
+
+class Street {
+	public static void main(String match[]){
+		System.out.println("i walking in the street");
+	}
+	
+}
