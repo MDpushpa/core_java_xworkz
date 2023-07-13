53e3f7ec29a37da9b5bde2cbb509320cbe7ab38c
mdpushpa <pushpamd575@gmail.com>
2023-07-13 17:36:47 +0530

    add traffic

--- Hotel.java
+++
@@ -1,6 +0,0 @@
-class Hotel{
-        public static void main(String arg[]) {
-		   System.out.println("i am going to hotel to eat a food ");
-		}
-		  
-}
---
+++ Traffic.java
@@ -0,0 +1,6 @@
+class Traffic{
+        public static void main(String arg[]) {
+		   System.out.println("bang have a lot of traffic");
+		}
+		  
+}
