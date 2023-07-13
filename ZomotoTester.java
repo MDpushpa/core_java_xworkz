0eaa234faf15f5b69cc8e8d2ec03a5891c23d19b
mdpushpa <pushpamd575@gmail.com>
2023-07-13 14:20:11 +0530

    adding Zomoto files method

---
+++ ZomotoTester.java
@@ -0,0 +1,146 @@
+class ZomotoTester{
+
+public static void main(String foodDelivery[]){
+	System.out.println("the avilabal foods are");
+	String foodName = "pizza";
+//double priceOfFoodName = Zomoto.search(foodName);
+//System.out.println(priceOfFoodName);
+double priceWithGivenQuantity = Zomoto.search(foodName,4);
+System.out.println("the food price with quantity"+priceWithGivenQuantity);
+
+
+String foodName1 = "biryani";
+double priceOfFoodName1 = Zomoto.search(foodName1);
+System.out.println(priceOfFoodName1);
+double priceWithGivenQuantity1 = Zomoto.search(foodName1,2);
+System.out.println("the food price with quantity"+priceWithGivenQuantity1);
+
+String foodName2 = "palavu";
+double priceOfFoodName2 = Zomoto.search(foodName2);
+System.out.println(priceOfFoodName2);
+double priceWithGivenQuantity2 = Zomoto.search(foodName2,5);
+System.out.println("the food price with quantity"+priceWithGivenQuantity2);
+
+String foodName3 = "puliyogare";
+double priceOfFoodName3 = Zomoto.search(foodName3);
+System.out.println(priceOfFoodName3);
+double priceWithGivenQuantity3 = Zomoto.search(foodName3,2);
+System.out.println("the food price with quantity"+priceWithGivenQuantity3);
+
+String foodName4 = "dose";
+double priceOfFoodName4 = Zomoto.search(foodName4);
+System.out.println(priceOfFoodName4);
+double priceWithGivenQuantity4 = Zomoto.search(foodName4,6);
+System.out.println("the food price with quantity"+priceWithGivenQuantity4);
+
+String foodName5 = "idly";
+double priceOfFoodName5 = Zomoto.search(foodName5);
+System.out.println(priceOfFoodName5);
+double priceWithGivenQuantity5 = Zomoto.search(foodName5,3);
+System.out.println("the food price with quantity"+priceWithGivenQuantity5);
+
+String foodName6 = "puri";
+double priceOfFoodName6 = Zomoto.search(foodName6);
+System.out.println(priceOfFoodName6);
+double priceWithGivenQuantity6 = Zomoto.search(foodName6,5);
+System.out.println("the food price with quantity"+priceWithGivenQuantity6);
+
+String foodName7= "pani puri";
+double priceOfFoodName7 = Zomoto.search(foodName7);
+System.out.println(priceOfFoodName7);
+double priceWithGivenQuantity7 = Zomoto.search(foodName7,3);
+System.out.println("the food price with quantity"+priceWithGivenQuantity7);
+
+String foodName8 = "gobi";
+double priceOfFoodName8 = Zomoto.search(foodName8);
+System.out.println(priceOfFoodName8);
+double priceWithGivenQuantity8 = Zomoto.search(foodName8,6);
+System.out.println("the food price with quantity"+priceWithGivenQuantity8);
+
+String foodName9 = "panier masala";
+double priceOfFoodName9 = Zomoto.search(foodName9);
+System.out.println(priceOfFoodName9);
+double priceWithGivenQuantity9 = Zomoto.search(foodName9,7);
+System.out.println("the food price with quantity"+priceWithGivenQuantity9);
+
+
+String foodName10 = "egg rice";
+double priceOfFoodName10 = Zomoto.search(foodName10);
+System.out.println(priceOfFoodName10);
+double priceWithGivenQuantity10 = Zomoto.search(foodName10,6);
+System.out.println("the food price with quantity"+priceWithGivenQuantity10);
+
+
+String foodName11 = "veg biriyani";
+double priceOfFoodName11 = Zomoto.search(foodName11);
+System.out.println(priceOfFoodName11);
+double priceWithGivenQuantity11 = Zomoto.search(foodName11,4);
+System.out.println("the food price with quantity"+priceWithGivenQuantity11);
+
+
+String foodName12 = "curd rice";
+double priceOfFoodName12 = Zomoto.search(foodName12);
+System.out.println(priceOfFoodName12);
+double priceWithGivenQuantity12= Zomoto.search(foodName12,5);
+System.out.println("the food price with quantity"+priceWithGivenQuantity12);
+
+
+String foodName13 = "rotti";
+double priceOfFoodName13 = Zomoto.search(foodName13);
+System.out.println(priceOfFoodName13);
+double priceWithGivenQuantity13 = Zomoto.search(foodName13,2);
+System.out.println("the food price with quantity"+priceWithGivenQuantity13);
+
+String foodName14 = "rave idly";
+double priceOfFoodName14 = Zomoto.search(foodName14);
+System.out.println(priceOfFoodName14);
+double priceWithGivenQuantity14 = Zomoto.search(foodName14,4);
+System.out.println("the food price with quantity"+priceWithGivenQuantity14);
+
+String foodName15 = "puri sagu";
+double priceOfFoodName15 = Zomoto.search(foodName15);
+System.out.println(priceOfFoodName15);
+double priceWithGivenQuantity15 = Zomoto.search(foodName15,6);
+System.out.println("the food price with quantity"+priceWithGivenQuantity15);
+
+
+String foodName16 = "juice";
+double priceOfFoodName16 = Zomoto.search(foodName16);
+System.out.println(priceOfFoodName16);
+double priceWithGivenQuantity16 = Zomoto.search(foodName16,8);
+System.out.println("the food price with quantity"+priceWithGivenQuantity16);
+
+
+String foodName17 = "frid rice";
+double priceOfFoodName17 = Zomoto.search(foodName17);
+System.out.println(priceOfFoodName17);
+double priceWithGivenQuantity17 = Zomoto.search(foodName17,1);
+System.out.println("the food price with quantity"+priceWithGivenQuantity17);
+
+
+String foodName18 = "apple";
+double priceOfFoodName18 = Zomoto.search(foodName18);
+System.out.println(priceOfFoodName18);
+double priceWithGivenQuantity18 = Zomoto.search(foodName18,5);
+System.out.println("the food price with quantity"+priceWithGivenQuantity18);
+
+
+String foodName19 = "banana";
+double priceOfFoodName19 = Zomoto.search(foodName19);
+System.out.println(priceOfFoodName19);
+double priceWithGivenQuantity19 = Zomoto.search(foodName19,3);
+System.out.println("the food price with quantity"+priceWithGivenQuantity19);
+
+String foodName20 = "orange";
+double priceOfFoodName20 = Zomoto.search(foodName20);
+System.out.println(priceOfFoodName20);
+double priceWithGivenQuantity20 = Zomoto.search(foodName20,7);
+System.out.println("the food price with quantity"+priceWithGivenQuantity20);
+
+}
+
+
+
+
+
+}
